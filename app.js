d34ee022f04152d3c05a0a4ceed3540feb74bbb7
Maura Slavin <MauraMSlavin@gmail.com>
2019-12-04 13:22:37 -0500

    Created Intern constructor.  Intern tests passed.  All tests passed.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const inquirer = require("inquirer");
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 
 let newEmployee = new Employee("Maura", 0, "Maura@gmail", "big boss");
