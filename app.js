2dce7af8cf98d321e84df17fb5d95bd9204cf8d1
Maura Slavin <MauraMSlavin@gmail.com>
2019-12-04 23:38:27 -0500

    Builds and opens html page with test data.

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // inquirer is used to prompt the user for employee information
 const inquirer = require("inquirer");
 // open to open the html file when done
-const open = require('open');
+const open = require("open");
 
 // pull in object constructors
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+// pull in writeHtml code
+const writeHtml = require("./lib/writeHtml");
 
 // test data
 let employees = [
@@ -43,8 +46,9 @@ let employees = [
 
 // let newEmployee = new Employee("Maura", 0, "Maura@gmail", "big boss");
 // console.log(newEmployee);
-console.log("employees:");
-console.log(employees);
-// writeHtml.write(employees);
+// console.log("employees:");
+// console.log(employees);
 
-open("./Assets/output/test.html");
+writeHtml.write(employees);
+
+open("./Assets/output/output.html");
