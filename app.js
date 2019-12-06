d6419561c71aa5434214783131cfa71df24796fb
Maura Slavin <MauraMSlavin@gmail.com>
2019-12-06 15:50:05 -0500

    Independent function ask.js asks needed questions.  Need to include as module.

--- app.js
+++ app.js
@@ -8,46 +8,44 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Ask = require("./lib/Ask");
 
 // pull in writeHtml code
 const writeHtml = require("./lib/writeHtml");
 
 // test data
 let employees = [
-    {
-    name: "Maura",
-    id: 0,
-    email: "this@email",
-    title: "Manager",
-    officeNumber: 222
-},
-    {
-    name: "Danielle",
-    id: 1,
-    email: "that@email",
-    title: "Engineer",
-    github: "D Slavin"
-},
-    {
-    name: "Mike",
-    id: 2,
-    email: "another@email",
-    title: "Engineer",
-    github: "M Slavin"
-},
-    {
-    name: "Camden",
-    id: 3,
-    email: "unh@email",
-    title: "Intern",
-    school: "UNH"
-}
+//     {
+//     name: "Maura",
+//     id: 0,
+//     email: "this@email",
+//     title: "Manager",
+//     officeNumber: 222
+// },
+//     {
+//     name: "Danielle",
+//     id: 1,
+//     email: "that@email",
+//     title: "Engineer",
+//     github: "D Slavin"
+// },
+//     {
+//     name: "Mike",
+//     id: 2,
+//     email: "another@email",
+//     title: "Engineer",
+//     github: "M Slavin"
+// },
+//     {
+//     name: "Camden",
+//     id: 3,
+//     email: "unh@email",
+//     title: "Intern",
+//     school: "UNH"
+// }
 ];
 
-// let newEmployee = new Employee("Maura", 0, "Maura@gmail", "big boss");
-// console.log(newEmployee);
-// console.log("employees:");
-// console.log(employees);
+Ask.ask();
 
 writeHtml.write(employees);
 
